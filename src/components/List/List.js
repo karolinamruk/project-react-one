75405e1091704aebd1c7eaf0dd9e5fe8dcd3537c
Karolina Mruk <kmruk88@gmail.com>
2024-07-11 08:23:15 +0200

    Initial commit

---
+++ src/components/List/List.js
@@ -0,0 +1,25 @@
+import styles from './List.module.scss';
+import Column from '../Column/Column.js';
+
+const List = (props) => {
+  return (
+    <div className={styles.list}>
+      <header className={styles.header}>
+        <h2 className={styles.title}>
+          Things to do <span>soon</span>
+        </h2>
+      </header>
+
+      <p className={styles.description}>
+        "Interesting thinks I want to check out"
+      </p>
+      <section className={styles.columns}>
+        <Column title="Books" icon="book" />
+        <Column title="Movies" icon="film" />
+        <Column title="Games" icon="gamepad" />
+      </section>
+    </div>
+  );
+};
+
+export default List;
---
+++ src/components/SearchForm/SearchForm.js
@@ -0,0 +1,18 @@
+import styles from './SearchForm.module.scss';
+import TextInput from '../TextInput/TextInput.js';
+import Button from '../Button/Button.js';
+
+const SearchForm = () => {
+  return (
+    <form className={styles.searchForm}>
+      <TextInput placeholder="Search..." />
+
+      {/* <Button text="Search" /> */}
+      <Button>
+        <span className="fa fa-search" />
+      </Button>
+    </form>
+  );
+};
+
+export default SearchForm;
