163e7ee754783a93b2289f8d37eb1ceedc2bd1d9
Karolina Mruk <kmruk88@gmail.com>
2024-07-24 15:28:41 +0200

    Add functionality for reading data from storage

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,90 +1,83 @@
 import styles from './List.module.scss';
 import Column from '../Column/Column.js';
-import { useState } from 'react';
+// import { useState } from 'react';
 import ColumnForm from '../ColumnForm/ColumnForm';
-import shortid from 'shortid';
+// import shortid from 'shortid';
+import { useSelector } from 'react-redux';
 
 const List = () => {
-  const [columns, setColumns] = useState([
-    {
-      id: 1,
-      title: 'Books',
-      icon: 'book',
-      cards: [
-        { id: 1, title: 'This is Going to Hurt' },
-        { id: 2, title: 'Interpreter of Maladies' },
-      ],
-    },
-    {
-      id: 2,
-      title: 'Movies',
-      icon: 'film',
-      cards: [
-        { id: 1, title: 'Harry Potter' },
-        { id: 2, title: 'Star Wars' },
-      ],
-    },
-    {
-      id: 3,
-      title: 'Games',
-      icon: 'gamepad',
-      cards: [
-        { id: 1, title: 'The Witcher' },
-        { id: 2, title: 'Skyrim' },
-      ],
-    },
-  ]);
+  const columns = useSelector((state) => state.columns);
+  // const [columns, setColumns] = useState([
+  //   {
+  //     id: 1,
+  //     title: 'Books',
+  //     icon: 'book',
+  //     cards: [
+  //       { id: 1, title: 'This is Going to Hurt' },
+  //       { id: 2, title: 'Interpreter of Maladies' },
+  //     ],
+  //   },
+  //   {
+  //     id: 2,
+  //     title: 'Movies',
+  //     icon: 'film',
+  //     cards: [
+  //       { id: 1, title: 'Harry Potter' },
+  //       { id: 2, title: 'Star Wars' },
+  //     ],
+  //   },
+  //   {
+  //     id: 3,
+  //     title: 'Games',
+  //     icon: 'gamepad',
+  //     cards: [
+  //       { id: 1, title: 'The Witcher' },
+  //       { id: 2, title: 'Skyrim' },
+  //     ],
+  //   },
+  // ]);
 
-  const addColumn = (newColumn) => {
-    setColumns([
-      ...columns,
-      {
-        id: shortid(),
-        title: newColumn.title,
-        icon: newColumn.icon,
-        cards: [],
-      },
-    ]);
-  };
+  // const addColumn = (newColumn) => {
+  //   setColumns([
+  //     ...columns,
+  //     {
+  //       id: shortid(),
+  //       title: newColumn.title,
+  //       icon: newColumn.icon,
+  //       cards: [],
+  //     },
+  //   ]);
+  // };
 
-  const addCard = (newCard, columnId) => {
-    const columnsUpdated = columns.map((column) => {
-      if (column.id === columnId)
-        return {
-          ...column,
-          cards: [...column.cards, { id: shortid(), title: newCard.title }],
-        };
-      else return column;
-    });
+  // const addCard = (newCard, columnId) => {
+  //   const columnsUpdated = columns.map((column) => {
+  //     if (column.id === columnId)
+  //       return {
+  //         ...column,
+  //         cards: [...column.cards, { id: shortid(), title: newCard.title }],
+  //       };
+  //     else return column;
+  //   });
 
-    setColumns(columnsUpdated);
-  };
+  //   setColumns(columnsUpdated);
+  // };
 
   return (
     <div className={styles.list}>
       <header className={styles.header}>
         <h2 className={styles.title}>
-          Things to do <span>soon</span>
+          Things to do<span>soon!</span>
         </h2>
       </header>
-
       <p className={styles.description}>
-        "Interesting thinks I want to check out"
+        Interesting things I want to check out
       </p>
-
       <section className={styles.columns}>
         {columns.map((column) => (
-          <Column
-            key={column.id}
-            title={column.title}
-            icon={column.icon}
-            cards={column.cards}
-            addCard={addCard}
-            id={column.id}
-          />
+          <Column key={column.id} {...column} />
         ))}
       </section>
-      <ColumnForm action={addColumn} />
+      <ColumnForm />
     </div>
   );
 };
