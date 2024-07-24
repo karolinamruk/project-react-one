6b3f6be639e970af8c1c86b0571c94a57fd7739c
Karolina Mruk <kmruk88@gmail.com>
2024-07-24 20:41:34 +0200

    Data modification

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,45 @@
 import { createStore } from 'redux';
 import initialState from './initialState.js';
+import shortid from 'shortid';
+
+// const reducer = (state, action) => {
+//   return state;
+// };
 
 const reducer = (state, action) => {
-  return state;
+  // const reducer = (state = initialState, action) => {
+  //   switch (action.type) {
+  //     case 'ADD_COLUMN': {
+
+  //       const maxColumnId = state.columns.reduce((maxId, column) => Math.max(maxId, column.id), 0);
+  //       const newColumnId = maxColumnId + 1;
+
+  //       const newColumn = {
+  //         id: newColumnId,
+  //         ...action.payload,
+  //       };
+
+  //       return {
+  //         ...state,
+  //         columns: [...state.columns, newColumn],
+  //       };
+  //     }
+
+  switch (action.type) {
+    case 'ADD_COLUMN':
+      return {
+        ...state,
+        columns: [...state.columns, { id: shortid(), ...action.payload }],
+      };
+
+    case 'ADD_CARD':
+      return {
+        ...state,
+        cards: [...state.cards, { ...action.payload }],
+      };
+    default:
+      return state;
+  }
 };
 
 const store = createStore(
